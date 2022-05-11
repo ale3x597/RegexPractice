224090720cc59cfb71a20635e802d6a3b250cf33
ale3x597 <90639588+ale3x597@users.noreply.github.com>
2022-05-10 23:36:18 -0400

    completed a short script test which adds a class valid to element when the telephone regex is fulfilled

---
+++ script.js
@@ -0,0 +1,29 @@
+// these are the 2 ways to make a regular expression in js
+//const reg =/[a-z]/gi;
+
+//const other_reg = new RegExp(/[a-z]/, 'i');
+
+
+const inputs = document.querySelectorAll('input');
+
+const patterns  = {
+    telephone: /^\d{11}$/, 
+}
+// cannot use inputs.addEventListener because variable inputs is a collection of All
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', (e)=>{
+        
+        //console.log(e.target.attributes.name.value);
+        valid(e.target,patterns[e.target.attributes.name.value])
+    });
+});
+
+function valid(field, regex){
+    //places a valid or invalid classname onto element if regex.test returns true
+    if (regex.test(field.value)){
+        field.className='valid'
+    }else {
+        field.className='invalid'
+    };
+}
